62841ff742d63d1b4d5de96ecde020abe81b44eb
Martynas Kalenda <martynjux@gmail.com>
2025-05-17 18:38:01 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
-import viteLogo from '/coq-inu-coin-svg.svg'
-import './App.scss'
-import Dialog from './components/dialog/dialog'
+import { useState } from 'react';
+import viteLogo from '/coq-inu-coin-svg.svg';
+import './App.scss';
+import Dialog from './components/dialog/dialog';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="dialog">
-      <Dialog/>
-    </div>
-      <img src='./coq-inu-coin-svg.svg' alt='doggo'></img>
+      <div className="dialog">
+        <Dialog />
+        <nav>
+          <ul>
+            <li>home</li>
+            <li>gallery</li>
+            <li>about us</li>
+            <li>find us</li>
+          </ul>
+        </nav>
+      </div>
+      <img src="./coq-inu-coin-svg.svg" alt="doggo"></img>
       <h1>coq-inu-coin</h1>
       <p>stay tuned</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
